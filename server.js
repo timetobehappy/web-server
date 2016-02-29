83fbb3a8a71ddcf254cb46b082a5ef8df166606c
Common Doubts <jibiuthaman@gmail.com>
2016-02-29 10:13:03 -0600

    Updated language

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(middleware.logger); // App level middleware say for logging web requests
 })*/
 
 app.get('/about', middleware.requireAuthentication, function(req, res){ // route level middleware for say logging this particulat web request
-	res.send('About Us');
+	res.send('About Us!');
 })
 
 app.use(express.static(__dirname + '/public'));
