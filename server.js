acc7d003ece6a71b0316a8592e7a7b826f6d163b
timetobehappy <jibiu@outlook.com>
2016-03-01 19:54:04 -0600

    Revert "Refactor middleware"
    This reverts commit 71d567770afcc655451555b59d36b4e2788b985d.

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = require('./middleware.js');
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+
+		next();
+	}
+
+};
 
 app.use(middleware.logger); // App level middleware say for logging web requests
 
